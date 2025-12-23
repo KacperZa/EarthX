4e4619bcd6277bd96020bf6ad95bbb92767354a8
KacperZa <lacper.zajac765@gmail.com>
2025-12-23 10:07:59 +0100

    Minor changes

--- globe-app/src/components/Europe.jsx
+++ globe-app/src/components/Europe.jsx
@@ -1,18 +1,69 @@
 import React, { useState } from 'react'
 import './continent2.css'
 import { motion, AnimatePresence, delay, transform } from 'framer-motion'
-import ateny from '../ateny.jpg'
-import CircularGallery from './CircularGallery'
+
 import chorwacja from '.././chorwacja.jpg'
 import hiszpania from '.././hiszpania.png'
 import islandia from '.././islandia.png'
-import EuropeContent from './EuropeContent'
+import CloseButton from './CloseButton'
+import SplitText from './SplitText'
 
+import Starozytnosc from './EuropaEpoki/Starozytnosc'
+import Średniowiecze from './EuropaEpoki/Średniowiecze'
+import Nowożytność from './EuropaEpoki/Nowożytność'
+import Współczesność from './EuropaEpoki/Współczesność'
 const Europe = ({style, setEuropa, Europa}) => {
   const [EuropeNews, setEuropeNews] = useState(false)
   const [Tytul, setTytul] = useState(true)
+
+  const [isCloseButtonVisible, setCloseButtonVisible] = useState(false)
+  const [ŚredniowieczeVisible, setŚredniowieczeVisible] = useState(false)
+  const [StarożytnośćVisible, setStarożytnoścVisible] = useState(false)
+  const [NowożytnośćVisible, setNowożytnośćVisible] = useState(false)
+  const [WspółczesnośćVisible, setWspółczesnośćVisible] = useState(false)
+
+  const [text, setText] = useState('')
+  const [Wartosc, setWartosc] = useState(0)
+
+  const ValueChangeHandler = (e) =>{
+    const Rok = Number(e.target.value);
+
+
+      if (!Rok) {
+        setStarożytnoścVisible(true)
+      }
+
+      if (Rok < 476) {
+        setStarożytnoścVisible(true)
+        setŚredniowieczeVisible(false)
+        setNowożytnośćVisible(false)
+        setWspółczesnośćVisible(false)
+      }
+      if (Rok >= 476 && Rok < 1492) {
+        setŚredniowieczeVisible(true)
+        setStarożytnoścVisible(false)
+        setNowożytnośćVisible(false)
+        setWspółczesnośćVisible(false)
+      }
+      if (Rok >= 1492 && Rok < 1914) {
+        setNowożytnośćVisible(true)
+        setŚredniowieczeVisible(false)
+        setStarożytnoścVisible(false)
+        setWspółczesnośćVisible(false)
+      }
+      if (Rok >= 1914) {
+        setWspółczesnośćVisible(true)
+        setNowożytnośćVisible(false)
+        setŚredniowieczeVisible(false)
+        setStarożytnoścVisible(false)
+      }
+      
+}
+  
+
   return (
     <>
+    
     <AnimatePresence>
       { Tytul && (
         <motion.div
@@ -39,7 +90,12 @@ const Europe = ({style, setEuropa, Europa}) => {
             <h1 id="title">EUROPA</h1>
         </motion.div>
           <motion.div className="przycisk">
-            <motion.button className='close' onClick={() => {setEuropeNews(true);   setTytul(false)}}>AHA</motion.button>
+            <motion.button className='close' id='close-czytaj'
+            whileHover={{
+              scale:0.9,
+              transition: {duration: 0.1}
+            }}  
+          transition={{duration: 0.5}} onClick={() => {setEuropeNews(true);   setTytul(false); setCloseButtonVisible(true)}}>Czytaj dalej</motion.button>
           <motion.button className="close"
            whileHover={{
             scale:0.9,
@@ -48,7 +104,7 @@ const Europe = ({style, setEuropa, Europa}) => {
           transition={{duration: 0.5}}
           onClick={() => {setTytul(false); setTimeout(() => {
             setEuropa(false)
-          }, 300);}}>Close
+          }, 300);}}>Zamknij
           </motion.button>
           </motion.div>
           </motion.div>
@@ -67,18 +123,81 @@ const Europe = ({style, setEuropa, Europa}) => {
         }} 
         style={style}
         className='content'>
-          <div className='test1'>siema</div>
-          <div className='test1'>AHA</div>
 
-          <motion.div className='przycisk' id='close-main'>
-            <motion.button onClick={()=> {setEuropeNews(false); setTimeout(() => {
-              setEuropa(false)
-            }, 300);}} className='close'> Close</motion.button>
-          </motion.div>
+      <motion.div id="container-content">
+        {/* Panel powitalny(wstęp) */}
+        <div className="panel1">
+          <SplitText
+              text="Historia Europy: Od Antyku do Współczesności"
+              className='poczatek'
+              delay={100}
+              duration={2.5}
+              ease="power3.out"
+              splitType="words"
+              from={{ opacity: 0, y: 40 }}
+              to={{ opacity: 1, y: 0 }}
+              threshold={0.1}
+              rootMargin="-100px"
+              textAlign="center"
+          />
+        </div>
+        {/* Panel z informacjami o europie */}
+        <motion.div className="panel2">
+          <div className="naglowek-panel2">
+            1. Starożytność: Fundamenty Zachodu
+          </div>
+          <div className="container-panel2">
+            <div className="tekst-panel2">
+              
+              <div className="tekst1-panel2">
+                Dziedzictwo Grecji: To starożytni Grecy dali Europie to, co najcenniejsze: demokrację, filozofię, teatr i kanony piękna w architekturze. Ateny stały się pierwszym laboratorium wolnej myśli, gdzie pytania o naturę świata i człowieka zadawali Sokrates czy Platon.
+              </div>
+
+              <div className="tekst2-panel2">
+                Potęga Rzymu: Rzymianie przejęli grecką kulturę i połączyli ją z militarną potęgą oraz geniuszem inżynieryjnym. Stworzyli Imperium Rzymskie, które zjednoczyło większość kontynentu pod jednym prawem. Rzymskie drogi, akwedukty i – przede wszystkim – kodeksy prawne stały się szkieletem przyszłej Europy. U schyłku tej epoki kluczową rolę zaczęło odgrywać chrześcijaństwo, stając się spoiwem nowej rzeczywistości.
+              </div>
+
+            </div>
+            <div className="zdjecia-panel2">
+            </div>
+          </div>
+        </motion.div>
+
+        <div className="panel2" id='panel-timeline'>
+          <div className="opakowanie-tresc">
+
+            <div className="tresc">
+
+              { StarożytnośćVisible ? <Starozytnosc/>: null}
+              { NowożytnośćVisible ? <Nowożytność/>: null}
+              { WspółczesnośćVisible ? <Współczesność/>: null}
+              { ŚredniowieczeVisible ? <Średniowiecze/>: null}
+            </div>
+
+          </div>
+          <div className="opakowanie-timeline">
+            <label htmlFor="os-czasu"> Oś czasu europy</label>
+              <input type="range" name="" id="os-czasu" min={-800} max={2025}  onChange={ValueChangeHandler}/>
+          </div>
+
+        </div>
+        {/* <div className="panel2">
+
+        </div> */}
+      </motion.div>
+
         </motion.div>
           )}
       </AnimatePresence>
-
+      <AnimatePresence>
+      {isCloseButtonVisible && (
+        <CloseButton
+        setEuropa={setEuropa} 
+        Europa={Europa} 
+        setEuropeNews={setEuropeNews} 
+        setCloseButtonVisible={setCloseButtonVisible}
+        />)}
+      </AnimatePresence>
       </>
   )
 }
