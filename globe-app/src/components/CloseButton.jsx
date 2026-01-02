48e9da8e466fd9e0122a65a612b92d1bb0d5781c
KacperZa <lacper.zajac765@gmail.com>
2026-01-02 20:27:39 +0100

    Minor fixes

---
+++ globe-app/src/components/CloseButton.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './continent2.css'
+import { AnimatePresence, motion } from "framer-motion";
+
+const CloseButton = ({setEuropeNews, setEuropa, Europa, EuropeNews, setCloseButtonVisible}) => {
+  return (
+    <>
+    {/* <motion.div className="przycisk-content"> */}
+    {/* <AnimatePresence> */}
+    <motion.button className="close-content"
+    whileHover={{
+        scale:0.9,
+        transition: {duration: 0.1}
+    }}
+    initial = {{opacity: 0, scale: 0.8}}
+    animate= {{opacity:1, scale:1}}
+    exit = {{opacity:0, y: -1000}}
+    transition={{
+        scale: { type: "spring", bounce: 0.4, duration: 0.5},
+        opacity: { duration: 0.4, ease: "easeInOut"},
+        y:{ duration: 0.5, ease: "easeInOut" }
+    }} 
+    onClick={()=> {setEuropeNews(false); setCloseButtonVisible(false) ;setTimeout(() => {
+        setEuropa(false)
+    }, 300);}}>✕
+    </motion.button>
+    {/* </AnimatePresence> */}
+    {/* </motion.div> */}
+    </>
+  )
+}
+
+export default CloseButton
