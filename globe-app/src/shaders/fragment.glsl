1e5f937c7340069a714882643cba2b21cd6cf391
KacperZa <lacper.zajac765@gmail.com>
2026-02-20 13:17:19 +0100

    initial commit

---
+++ globe-app/src/shaders/fragment.glsl
@@ -0,0 +1,15 @@
+uniform sampler2D uTexture;
+uniform float uTime;
+
+varying vec3 vPosition;
+varying vec3 vNormal;
+varying vec2 vUv;
+
+
+
+void main() {
+
+
+    vec4 color = texture2D(uTexture, vUv);
+    gl_FragColor = vec4(color.xyz, 1);
+}
